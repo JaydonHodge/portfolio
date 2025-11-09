cc93428d9a8a1084902d098ccc078ad392196c35
BlazingJaydon <jaydonhodge@gmail.com>
2025-11-08 22:01:54 -0500

    whoops. added wrond dir to commit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,30 +3,32 @@ import '../styles/Nav.css'
 function Nav() {
   return (
     <section className="nav-bar">
-      <div className='nav-content'>
-        <div className="logo">
-          <a id="logo-anch" href="#home">
-            <img src="/portfolio/images/bolt.png" alt="Jaydon Hodge portfolio logo" />
-          </a>
+      <div className='nav-container'>
+        <div className='nav-content'>
+          <div className="logo">
+            <a id="logo-anch" href="#home">
+              <img src="/portfolio/images/logo.png" alt="Jaydon Hodge portfolio logo" />
+            </a>
+          </div>
+          <ul className="nav-list">
+            <li><a href="#about" className="page-links about"><span className="nums">01.</span><span className="items">About</span></a></li>
+
+            <li><a href="#experience" className="page-links experience"><span className="nums">02.</span><span className="items">Experience</span></a></li>
+
+            <li><a href="#projects" className="page-links projects"><span className="nums">03.</span><span className="items">Projects</span></a></li>
+
+            <li><a href="#contact" className="page-links contact"><span className="nums">04.</span><span className="items">Contact</span></a></li>
+
+            <li>
+              <button className="resume" onClick={() => window.open(`${import.meta.env.BASE_URL}jaydonhodge_resume_i.pdf`, "_blank")}>
+                Resume
+              </button>
+            </li>
+          </ul>
         </div>
-        <ul className="nav-list">
-          <li><a href="#about" className="page-links about"><span className="nums">01.</span><span className="items">About</span></a></li>
 
-          <li><a href="#experience" className="page-links experience"><span className="nums">02.</span><span className="items">Experience</span></a></li>
-
-          <li><a href="#projects" className="page-links projects"><span className="nums">03.</span><span className="items">Projects</span></a></li>
-
-          <li><a href="#contact" className="page-links contact"><span className="nums">04.</span><span className="items">Contact</span></a></li>
-
-          <li>
-            <button className="resume" onClick={() => window.open(`${import.meta.env.BASE_URL}jaydonhodge_resume_i.pdf`, "_blank")}>
-              Resume
-            </button>
-          </li>
-        </ul>
+        <div className="nav-divide" id="nav-divide" />
       </div>
-
-      <div className="nav-divide" id="nav-divide" />
     </section>
   )
 }
