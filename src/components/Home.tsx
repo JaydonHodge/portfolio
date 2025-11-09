072f4f69599c7fb8752382e7ea7d747d451c0a32
BlazingJaydon <jaydonhodge@gmail.com>
2025-11-09 11:15:54 -0500

    worked on home section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,20 +5,20 @@ function Home() {
     <section role='region' id='home'>
       <p>Hi, my name is</p>
       <h1 id="name">Jaydon Hodge.</h1>
-      <h1 id="alt-name">I also go by "Yajenio"</h1>
-      <h1 id="joke-intro">I'm a CMPSC student by day, and a CMPSC enthusiast also by day</h1>
+      <h1 id="title">&lt;I am a Software Engineer/&gt;</h1>
+      <div className='cursor'>
+        <h1>_</h1>
+      </div>
       <div className="home-description">
         <p id="first">The purpose of this web page is to serve as my <span className="highlight">personal portfolio</span> and <span className="highlight">online presence hub</span>.
           It is meant to be a place to display my ventures within my tech career, as well as a place of information for anyone that is
           interested in learning more about who I am and what I do.
         </p>
       </div>
-      <div className="message-div">
-        <p className="message">
-          Head on down below and explore the rest of the page to find out
-          more!
-        </p>
-      </div>
+      <p className="message">
+        Head on down below and explore the rest of the page to find out
+        more!
+      </p>
     </section>
   )
 }
