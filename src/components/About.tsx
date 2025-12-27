d1d7f95d39faa0ea13bfcc6447e7581e68aca738
Jaydon Hodge <yajenio@mac.myfiosgateway.com>
2025-12-27 18:46:23 -0500

    finished about section. prob gonna be done with portfolio for a while

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,27 @@
 import '../styles/About.css'
 import { TiArrowDown } from "react-icons/ti";
+import { useState } from 'react';
+
+interface VidCardProps {
+    isVisible: boolean;
+}
+
+function VidCard({ isVisible }: VidCardProps) {
+    return <div className={`video ${isVisible ? 'show' : ''}`}>
+        <div className='youtube'>
+            <iframe src="https://www.youtube.com/embed/Y9iEiXVACxg?si=61cwoRyxTysNEC9o" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
+    </div>
+}
 
 function About() {
+    const [visible, setVisibility] = useState(false);
+    const [rotated, setRotation] = useState(false);
+
+    function handleClick() {
+        setVisibility(!visible);
+        setRotation(!rotated);
+    }
 
     return (
         <section role='region' id='about'>
@@ -21,9 +41,10 @@ function About() {
                     <span className='ps'>
 
                         <p>P.S. I'm also a <span className='bold'>huge</span> musicophile and right now I'm learning guitar :)</p>
-                        <button className='vid-link'>Take a look<TiArrowDown className='drop' /></button>
+                        <button className='vid-link' onClick={handleClick}>Take a look<TiArrowDown className={`drop ${rotated ? 'up' : ''}`} /></button>
                     </span>
 
+                    <VidCard isVisible={visible} />
                 </div>
             </div>
         </section>
