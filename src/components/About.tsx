8312e0004421ce9523c5037d1d1553b6bc62fcb4
Jaydon Hodge <yajenio@mac.myfiosgateway.com>
2025-12-26 21:58:53 -0500

    worked on about and experience sections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,12 @@
 import '../styles/About.css'
+import { TiArrowDown } from "react-icons/ti";
 
 function About() {
 
     return (
         <section role='region' id='about'>
             <div className='section-header'>
-                <h1><div className='nums'>01.</div>About</h1>
+                <h1><span className='nums'>01.</span>About</h1>
             </div>
             <div className='section-content'>
                 <div className='img-container'>
@@ -15,9 +16,13 @@ function About() {
                 <div className='about-text'>
                     <p>Hey Jaydon here! I'm a Full-Stack Software Engineer based in Baltimore. I recently graduated from The Pennsylvania State University with a Bachelor's in Computer Science. I am continuing my education with a Masters of Science in Data Science.</p>
 
-                    <p>For almost my entire life I've been into game development which is what drove me to pursue this sort of career in tech. So in my free time I improve my coding skills by tinkering and working on side projects that I think are fun and would be cool to see in some games</p>
+                    <p>For almost my entire life I've been into game development which is what drove me to pursue this sort of career in tech. So in my free time I like to improve my coding skills by tinkering and working on side projects that I think are fun and satisfy my love for games.</p>
 
-                    <p className='ps'>P.S. I'm also a <span className='bold'>huge</span> musicophile and am learning guitar right now :)</p>
+                    <span className='ps'>
+
+                        <p>P.S. I'm also a <span className='bold'>huge</span> musicophile and right now I'm learning guitar :)</p>
+                        <button className='vid-link'>Take a look<TiArrowDown className='drop' /></button>
+                    </span>
 
                 </div>
             </div>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,12 +1,36 @@
 import '../styles/Experience.css'
+import { FiArrowUpRight } from "react-icons/fi";
 
 function Experience() {
     return (
         <section role='region' id='experience'>
             <div className='section-header'>
-                <h1><div className='nums'>02.</div>Experience</h1>
+                <h1><span className='nums'>02.</span>Experience</h1>
             </div>
             <div className='section-content'>
+                <button className='card1'>
+                    <p className='date'>2024 ― 2026</p>
+                    <div className='job'>
+                        <h2 className='position'>Undergraduate Research Assistant · The Human In Computing &amp; Cognition Lab <FiArrowUpRight className='arrow' /> </h2>
+                        <div className='job-desc'>
+                            <li>
+                                Lead research trials for ongoing project for National Science Foundation where participants would interact and choose whether or not to cooperate with an AI agent of my team’s design.
+                            </li>
+                            <li>
+                                Coded and deployed Python scripts used for data preprocessing and analysis of data from research trials.
+                            </li>
+                            <li>
+                                Modified and extended the the JavaScript code used for the front-end user interface that participants of experiment interacted with.
+                            </li>
+                            <li>
+                                Conducted data analysis of the various different measurements from participants such as eye-tracking data and response time data.
+                            </li>
+                            <li>
+                                Derived and expanded Microsoft’s open-source Project Malmo for the backend for AI-Human Interaction.
+                            </li>
+                        </div>
+                    </div>
+                </button>
             </div>
         </section>
     )
